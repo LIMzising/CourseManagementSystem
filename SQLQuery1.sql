aeb2725845139dfba481c22dab8289021ebbd2f4
LIMzising <74854532+LIMzising@users.noreply.github.com>
2021-10-23 22:40:02 +0800

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE [dbo].[Locations] (
+	[LocationId] int IDENTITY(1,1) NOT NULL,
+	[LocationName] nvarchar(50) NOT NULL,
+	[LocationAddress] nvarchar(max) NOT NULL,
+	[LocationLatitude] NUMERIC(10, 8) NOT NULL,
+	[LocationLongitude] NUMERIC(11, 8) NOT NULL,
+	PRIMARY KEY (LocationId),
+	CONSTRAINT CK_Latitude CHECK (LocationLatitude >= -90 AND LocationLatitude <= 90),
+	CONSTRAINT CK_Longtitude CHECK (LocationLongitude >= -180 AND LocationLongitude <=180)
+);
+GO
+
+CREATE TABLE [dbo].[Trainers] (
+	[TrainerId] int IDENTITY(1,1) NOT NULL,
+	[TrainerName] nvarchar(50) NOT NULL,
+	[TrainerSpecialisation] nvarchar(max),
+	PRIMARY KEY (TrainerId)
+);
+GO
+
+CREATE TABLE [dbo].[Courses] (
+	[CourseId] int IDENTITY(1,1) NOT NULL,
+	[CourseName] nvarchar(50) NOT NULL,
+	[CourseDescription] nvarchar(max) NOT NULL,
+	[CourseCategory] nvarchar(50) NOT NULL,
+	[LocationId] int NOT NULL,
+	[TrainerId] int NOT NULL,
+	PRIMARY KEY (CourseId),
+	FOREIGN KEY (LocationId) REFERENCES Locations(LocationId),
+	FOREIGN KEY (TrainerId) REFERENCES Trainers(TrainerId)
+);
+GO
+
+CREATE TABLE [dbo].[Bookings] (
+	[BookingId] int IDENTITY(1,1) NOT NULL,
+	[BookingDate] date NOT NULL,
+	[CourseId] int NOT NULL,
+	[UserId] nvarchar(max) NOT NULL,
+	PRIMARY KEY (BookingId),
+	FOREIGN KEY (CourseId) REFERENCES Courses(CourseId)
+);
+GO
---
+++ TechnologyCourseSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnologyCourseSystem.Startup))]
+namespace TechnologyCourseSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
